9545473c76bc183dfd5185cbd1e29591f737a595
joshvanl <me@joshvanl.dev>
2023-02-28 16:49:41 +0000

    Adds gcloud image repo and sets version/tag to v0.1.0
    Signed-off-by: joshvanl <me@joshvanl.dev>

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
     src = nixpkgs.lib.sourceFilesBySuffices ./. [ ".go" "go.mod" "go.sum" "gomod2nix.toml" ];
     src-test = nixpkgs.lib.sourceFilesBySuffices ./test [ ".go" "go.mod" "go.sum" "gomod2nix.toml" ];
 
-    version = "0.1.0-alpha.0";
+    version = "0.1.0";
 
   in utils.lib.eachSystem targetSystems (system:
     let
